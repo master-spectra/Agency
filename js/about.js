81ca9af0beb49b81fc65e301c53dfd2553b31d40
master-spectra <63547718+master-spectra@users.noreply.github.com>
2020-05-10 14:45:13 +0300

    edit

--- js/about.js
+++ js/about.js
@@ -1,19 +1,9 @@
 $('.page .service-section .content').slick({
-    dots:true,
-    arrows:false,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    infinite:true,
-    autoplay: true,
-    autoplaySpeed: 2000
+  dots:true,
+  arrows:false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  infinite:true,
+  autoplay: true,
+  autoplaySpeed: 2000
 })
-ScrollOut({
-    onShown: function(el) {
-      // use the web animation API
-      el.animate([{ opacity: 0 }, { opacity: 1 }], 1000);
-    },
-    onHidden: function(el) {
-      // hide the element initially
-      el.style.opacity = 0;
-    }
-  });
