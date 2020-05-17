4fd916dc607a3894f1996db6999cc8bf6c95ce33
master-spectra <63547718+master-spectra@users.noreply.github.com>
2020-05-17 21:49:29 +0300

    edit

--- js/about.js
+++
@@ -1,9 +0,0 @@
-$('.page .service-section .content').slick({
-  dots:true,
-  arrows:false,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  infinite:true,
-  autoplay: true,
-  autoplaySpeed: 2000
-})
---
+++ js/main.js
@@ -0,0 +1,26 @@
+$('.page .service-section .content').slick({
+  dots:true,
+  arrows:false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  infinite:true,
+  autoplay: true,
+  autoplaySpeed: 2000
+})
+
+$('.page .slider .content-slider').slick({
+  arrows:false,
+  dots:true,
+  infinite:true,
+  autoplay: true,   
+})
+
+$('.page .works-list .wrapper .content').slick({
+    dots:true,
+    arrows:false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    infinite:true,
+    autoplay: true,
+    autoplaySpeed: 2000
+})
