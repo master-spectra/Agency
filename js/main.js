d84da3771ec2556c0d026b50aa2e60517d42a9ef
master-spectra <63547718+master-spectra@users.noreply.github.com>
2020-05-28 14:54:04 +0300

    edit

--- js/main.js
+++ js/main.js
@@ -1,33 +1,40 @@
+// animation fade element's
 AOS.init();
 
-$('.page .service-section .content').slick({
-  dots:true,
-  arrows:false,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  infinite:true,
-  autoplay: true,
-  autoplaySpeed: 2000
-})
-
-$('.page .slider .content-slider').slick({
-  arrows:false,
-  dots:true,
-  infinite:true,
-  autoplay: true,   
-})
-
-$('.page .works-list .wrapper .content').slick({
-    dots:true,
-    arrows:false,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    infinite:true,
-    autoplay: true,
-    autoplaySpeed: 2000
+$('.tab-container').tabPlugin({
+    speed: 300,
+    transition: 'linear'
 });
 
-var typed = new Typed('#typed', {
-    stringsElement: '#typed-strings',
-    typeSpeed: 30
-});
+// slider
+// $('.page .service-section .content').slick({
+//     dots: true,
+//     arrows: false,
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     infinite: true,
+//     autoplay: true,
+//     autoplaySpeed: 2000
+// })
+
+// $('.page .slider .content-slider').slick({
+//     arrows: false,
+//     dots: true,
+//     infinite: true,
+//     autoplay: true, 
+// })
+
+// $('.page .works-list .wrapper .content').slick({
+//     dots: true,
+//     arrows: false,
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     infinite: true,
+//     autoplay: true,
+//     autoplaySpeed: 2000
+// });
+
+// var typed = new Typed('#typed', {
+//     stringsElement: '#typed-strings',
+//     typeSpeed: 30
+// });
